0b2e2a8c260352c74d7be63e8c2dc45adf624e4b
canaantt <canaantt@gmail.com>
2017-08-21 19:45:13 -0700

    first two small apps

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Jenny Zhang on 8/21/17.
+//  Copyright © 2017 Jenny Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
